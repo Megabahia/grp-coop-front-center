d9a53df4047a651a04b09fee73d4c5ca0220585d
jimmy <rootkaliente@gmail.com>
2023-07-13 11:54:22 -0500

    [feature] mostrar archivos de firma documentos

--- src/app/main/central/vistas/center/solicitud-pago-empleados/solicitud-pago-empleados.component.ts
+++ src/app/main/central/vistas/center/solicitud-pago-empleados/solicitud-pago-empleados.component.ts
@@ -113,13 +113,13 @@ export class SolicitudPagoEmpleadosComponent implements OnInit, AfterViewInit {
     procesarPago(pago) {
         this.idPagoProveedor = pago._id;
         this.solicitudPago = pago;
-        this._solicitudesPagoEmpleadosService.obtenerInformacionCredito(pago.credito_id).subscribe((data) => {
-           this.solicitudPago.numeroCuenta = JSON.parse(data?.empresaEmpleado).numeroCuenta;
-           this.solicitudPago.titularCuenta = JSON.parse(data?.empresaEmpleado).titularCuenta;
-           this.solicitudPago.correoTitular = JSON.parse(data?.empresaEmpleado).correoTitular;
-           this.solicitudPago.bancoDestino = JSON.parse(data?.empresaEmpleado).bancoDestino;
-            this.abrirModal(this.procesarMdl);
-        });
+        if (pago?.casaComercial) {
+            this.solicitudPago.numeroCuenta = JSON.parse(pago?.casaComercial).numeroCuenta;
+            this.solicitudPago.titularCuenta = JSON.parse(pago?.casaComercial).titularCuenta;
+            this.solicitudPago.correoTitular = JSON.parse(pago?.casaComercial).correoTitular;
+            this.solicitudPago.bancoDestino = JSON.parse(pago?.casaComercial).bancoDestino;
+        }
+        this.abrirModal(this.procesarMdl);
     }
 
     transformarFecha(fecha) {
